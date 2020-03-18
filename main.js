a975e4540f8127f32625337e17ee15ec7036a535
apple <apple@Tankys-MacBook-Pro.local>
2020-03-18 21:54:22 +0800

    Mine blocks.

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 const SHA256 = require('crypto-js/sha256');
 
+class Transaction {
+    constructor(fromAddress, toAddress, amount) {
+        this.fromAddress = fromAddress;
+        this.toAddress = toAddress;
+    }
+}
+
 class Block {
     constructor(index, timestamp, data, previousHash = ''){
         this.index = index;
@@ -7,19 +14,29 @@ class Block {
         this.data = data;
         this.previousHash = previousHash;
         this.hash = '';
+        this.nonce = 0;
     }
 
     /**
      * Hash of index, previousHash, timestamp, data.
      */
     calculateHash(){
-        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
+    }
+
+    mineBlock(difficulty){
+        while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")){
+            this.nonce++;
+            this.hash = this.calculateHash();
+        }
+        console.log("Block mined:" + this.hash);
     }
 }
 
 class Blockchain{
     constructor(){
         this.chain = [this.createGenesisBlock()];
+        this.difficulty = 1;
     }
 
     /**
@@ -36,13 +53,15 @@ class Blockchain{
     }
 
     addBlock(newBlock){
-        console.log('newBlock');
+        console.log('addBlock', newBlock);
         newBlock.previousHash = this.getLatestBlock().hash;
-        newBlock.hash = newBlock.calculateHash();
+        newBlock.mineBlock(this.difficulty);
         this.chain.push(newBlock);
     }
 
     isChainValid() {
+        console.log('isChainValid');
+
         for(let i = 1; i < this.chain.length; i++) {
             const currentBlock = this.chain[i];
             const previousBlock = this.chain[i-1];
@@ -60,14 +79,32 @@ class Blockchain{
 }
 
 // Demo 1: Add block
-let xcoin = new Blockchain();
-xcoin.addBlock(new Block(1, "10/07/2017", { datakey: "datavalue"}));
-xcoin.addBlock(new Block(1, "10/07/2017", { datakey: "datavalue"}));
-
-console.log(JSON.stringify(xcoin, null, 4));
+function testAddBlock() {
+    let xcoin = new Blockchain();
+    xcoin.addBlock(new Block(1, "10/07/2017", { datakey: "datavalue"}));
+    xcoin.addBlock(new Block(1, "10/07/2017", { datakey: "datavalue"}));
+    
+    console.log(JSON.stringify(xcoin, null, 4));
+}
 
 // Demo 2: isChainValid
-console.log('BEFORE EDIT: xcoin.isChainValid()', xcoin.isChainValid());
-xcoin.chain[1].data = { amount: 123 };
-console.log('AFTER EDIT: xcoin.isChainValid()', xcoin.isChainValid());
+function testIsChainValid() {
+    console.log('BEFORE EDIT: xcoin.isChainValid()', xcoin.isChainValid());
+    xcoin.chain[1].data = { amount: 123 };
+    console.log('AFTER EDIT: xcoin.isChainValid()', xcoin.isChainValid());    
+}
 
+// Demo 3: mining
+function testMiningBasic() {
+    let xcoin = new Blockchain();
+
+    console.log('Mining block 1...');
+    
+    xcoin.addBlock(new Block(1, "17/03/2020", { amount: 4 }));
+    
+    console.log('Mining block 2...');
+    
+    xcoin.addBlock(new Block(2, "17/03/2020", { amount: 8 }));
+}
+
+// testMiningBasic();
